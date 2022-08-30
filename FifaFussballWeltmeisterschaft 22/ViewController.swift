114bfd057f9a623c17e632ec1d57eb484cacda83
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-08-30 11:55:08 +0200

    Initial Commit

---
+++ FifaFussballWeltmeisterschaft 22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FifaFussballWeltmeisterschaft 22
+//
+//  Created by Mirko Weitkowitz on 30.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
